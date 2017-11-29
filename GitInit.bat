632b620ef9df5da5b11e3401cb2f928afbc7f8a5
Kai47x <k.u.kietzke@gmail.com>
2017-11-29 23:58:00 +0100

    newbean

---
+++ GitInit.bat
@@ -0,0 +1,6 @@
+echo "# Blackbox 1.7rc2" >> README.md
+git init
+git add README.md
+git commit -m "initial commit"
+git remote add origin https://github.com/kai47x/Blackbox17.git
+git push -u origin master
---
+++ GitPush.bat
@@ -0,0 +1,6 @@
+echo "basics" >> README.md
+git init
+git add README.md
+git commit -m "added BBCenter-master vom 20. Nov 2017 1.7rc2"
+git remote add origin https://github.com/kai47x/Blackbox17.git
+git push -u origin master
